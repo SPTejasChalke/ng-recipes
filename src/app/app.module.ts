863eb72d1e6a06ef78670aae13d6ddc221cad9f4
Tejas Chalke <tejaschalke66@gmail.com>
2022-12-24 16:23:56 +0530

    conflicts resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,8 @@ import { ListItemComponent } from './list-item/list-item.component';
 import { AddListItemComponent } from './recipes/add-list-item/add-list-item.component';
 import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
 import { HomepageComponent } from './homepage/homepage.component';
-<<<<<<< HEAD
-=======
 import { ShopListItemComponent } from './shoplist/shop-list-item/shop-list-item.component';
 import { ShopdataService } from './shopdata.service';
->>>>>>> 6702abc4accb971a31fdf6e972d632058b24e0d5
 
 @NgModule({
   declarations: [
@@ -29,23 +26,15 @@ import { ShopdataService } from './shopdata.service';
     ListItemComponent,
     AddListItemComponent,
     RecipeDetailsComponent,
-<<<<<<< HEAD
-    HomepageComponent
-=======
     HomepageComponent,
     ShopListItemComponent
->>>>>>> 6702abc4accb971a31fdf6e972d632058b24e0d5
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-<<<<<<< HEAD
-  providers: [ReceipedataService],
-=======
   providers: [ReceipedataService, ShopdataService],
->>>>>>> 6702abc4accb971a31fdf6e972d632058b24e0d5
   bootstrap: [AppComponent]
 })
 export class AppModule { }
